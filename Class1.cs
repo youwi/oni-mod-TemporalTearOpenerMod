8c4fbfa0c2a239f35262afbc5f40873482301c9c
yu <yu@sec.com>
2022-10-04 06:54:04 +0800

    添加注释

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oni_mod_test
+{
+    internal class Class1
+    {
+        //https://code.ecool.dev/ONI-Mods/BuildableDirtTile/src/branch/master/BuildableDirtTilePatches.cs
+        //https://github.com/SanchozzDeponianin/ONIMods
+    }
+}
